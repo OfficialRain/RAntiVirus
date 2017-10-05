922e78f1afd121afae6630ceed9da4a784a0b69b
OfficialRain <ryanf0709@gmail.com>
2017-10-04 20:00:21 -0700

    Add the VB project! (with some things that we're still working on)

---
+++ Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RAntiVirus
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 36.0!, CType((System.Drawing.FontStyle.Italic Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Fuchsia
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(582, 68)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "WElcome tO RAntovIrsu"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(134, 268)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(617, 62)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "SCAN MY PC NOW!!!!!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(38, 100)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(155, 30)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "High Portectin"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(38, 137)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(147, 30)
+        Me.CheckBox2.TabIndex = 3
+        Me.CheckBox2.Text = "Low Potecton"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(38, 174)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(284, 30)
+        Me.CheckBox3.TabIndex = 4
+        Me.CheckBox3.Text = "No Protectin (not recomendd"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(602, 13)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(149, 240)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "PROFT DCETION STAUTS"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'RAntiVirus
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 26.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(763, 342)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Name = "RAntiVirus"
+        Me.Text = "RAntiVIrus betr v1.0.0.0.1.0.1 - I Got  A VIfre Starz Awasd From MY Mommu"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class RAntiVirus
+    Private Sub RAntiVirus_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("The following is a parody of most skiddie's AV applications. This does not detect and remove injections; this is a fake AV that is easy to remove. Have fun! - Rain and FDD")
+        MessageBox.Show("Hi Jus Letting you knOw tht This Anvit Iirsu findS Al Visus S Protsction Is AT The Max Ok?!?!?!? Thi sWas Codd in Vb And It is Bet Ativ IRus Alo Do no Close this WIndows!!!! It WIl l berak te AnitiIvus And IT will Stop Proecting you So You Shouldnt DO That Also You Should AWcitve Ur Product Because You Get EVEM ERPERS APROCETEDCTOP NASFGAOST FIVIESES!!!!1!1!!1 Ok Thas FoR Uderstmaom Have a nIce cay!1!!!!!")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Scanr.Show()
+    End Sub
+End Class
