ffadb22168a9c2ca6f4f5f7f95e29f3840e9922a
OfficialRain <ryanf0709@gmail.com>
2017-10-06 16:30:25 -0700

    Quick needed changes
    .NET Framework changed to 4.6.1 (for new dev to work on it).  Quick
    updates as well.

--- Virus1.Designer.vb
+++ Virus1.Designer.vb
@@ -23,6 +23,9 @@ Partial Class Virus1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -37,11 +40,49 @@ Partial Class Virus1
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "OH NOES A SVIRUS AWS AFOUND !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Comic Sans MS", 15.75!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(102, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(322, 29)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "!!!!!! VIRUS ALERT !!!!!! FOUND"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Comic Sans MS", 15.75!, CType((((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline) _
+                Or System.Drawing.FontStyle.Strikeout), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(13, 111)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(284, 54)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "DELETE THE VIRUS!!!1!1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Comic Sans MS", 15.75!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(328, 111)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(241, 72)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "DONT DELTE TH VIRUZ"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Virus1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(581, 261)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Virus1"
         Me.Text = "OH NY OD adA aCIVURSA WAS DESTCEDVTD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" &
@@ -52,4 +93,7 @@ Partial Class Virus1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
